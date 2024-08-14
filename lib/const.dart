611c5cf5aaaac11319ee650fda5b81c1a9b16f57
Danil Panarin <panarin.danil.08@gmail.com>
2024-08-15 03:18:34 +0500

    Новый файл для классов с цветами и иконками

---
+++ lib/const.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+class ColorInfo {
+  static const purpleLight = Color(0xFFF7EEFF);
+  static const purpleDark = Color(0xFFBA87FC);
+  static const purpleGreyLight = Color(0xFFF1EDF5);
+  static const purpleGreyDark = Color(0xFF817B89);
+  static const purpleDarkest = Color(0xFFA03FFF);
+  static const highlightDarkest = Color(0xFF006FFD);
+  static const errorDark = Color(0xFFED3241);
+  static const errorLight = Color(0xFFFFE2E5);
+  static const darkGreyLightest = Color(0xFF8F9098);
+  static const neutralDarkGreyDarkest = Color(0xFF1F2024);
+}
+
+class SvgInfo {
+  static const cross = "assets/svgs/cross.svg";
+  static const gymapplogo = "assets/svgs/gymapp_logo.svg";
+  static const gymatechlogo = "assets/svgs/gymatech_logo.svg";
+  static const leftarrow = "assets/svgs/left_arrow.svg";
+  static const telegramlogo = "assets/svgs/telegram_logo.svg";
+}
